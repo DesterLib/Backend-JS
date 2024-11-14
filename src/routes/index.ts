3c27c53ccd2242ab7423bcb7cc7e33d60cf66f18
AlkenD <desteralken@gmail.com>
2024-11-15 02:38:47 +0530

    feat: new metadata route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { ENV_API_VERSION } from "../config";
+
 import generateMetadataRoute from "./api/generate-metadata";
-import mediaRoute from "./api/media";
+import metadataRoute from "./api/metadata";
+import mediaRoute from "./api/file";
 
 const router = Router();
 
 router.use(ENV_API_VERSION + "/generate-metadata", generateMetadataRoute);
+router.use(ENV_API_VERSION + "/metadata", metadataRoute);
 router.use(ENV_API_VERSION + "/media", mediaRoute);
 
 export default router;
