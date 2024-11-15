419142000d00c2664b0a46bea148a61e42a1e0c7
AlkenD <desteralken@gmail.com>
2024-11-15 19:01:58 +0530

    feat: prisma schema migration from json to postgres db

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,6 @@ dotenv.config();
 
 export const ENV_PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 export const ENV_TMDB_API_KEY = process.env.TMDB_API_KEY || "";
-export const ENV_API_VERSION = process.env.API_VERSION || "";
-export const ENV_DEFAULT_METADATA_FOLDER = process.env.DEFAULT_METADATA_FOLDER_PATH || ""
+export const ENV_API_VERSION_PATH = "/api/v" + process.env.API_VERSION || "";
+export const ENV_DEFAULT_METADATA_FOLDER =
+  process.env.DEFAULT_METADATA_FOLDER_PATH || "";
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { ENV_API_VERSION } from "../config";
+import { ENV_API_VERSION_PATH } from "../config";
 
 import generateMetadataRoute from "./api/generate-metadata";
 import metadataRoute from "./api/metadata";
@@ -7,8 +7,8 @@ import mediaRoute from "./api/file";
 
 const router = Router();
 
-router.use(ENV_API_VERSION + "/generate-metadata", generateMetadataRoute);
-router.use(ENV_API_VERSION + "/metadata", metadataRoute);
-router.use(ENV_API_VERSION + "/media", mediaRoute);
+router.use(ENV_API_VERSION_PATH + "/generate-metadata", generateMetadataRoute);
+router.use(ENV_API_VERSION_PATH + "/metadata", metadataRoute);
+router.use(ENV_API_VERSION_PATH + "/media", mediaRoute);
 
 export default router;
